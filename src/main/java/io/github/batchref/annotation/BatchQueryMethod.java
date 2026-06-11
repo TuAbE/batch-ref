f7a633c1b8da6aa0c508ebd96991a2d496769c66
TaBuE <840545133@qq.com>
2026-06-11 21:26:50 +0800

    refactor(annotation): 移除BatchQueryMethod注解中的语言注解依赖
    - 移除了org.intellij.lang.annotations.Language注解
    - 简化了BatchQueryMethod注解的实现
    - 减少了注解的复杂性和外部依赖

--- src/main/java/io/github/batchref/annotation/BatchQueryMethod.java
+++ src/main/java/io/github/batchref/annotation/BatchQueryMethod.java
@@ -1,7 +1,5 @@
 package io.github.batchref.annotation;
 
-import org.intellij.lang.annotations.Language;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -11,10 +9,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface BatchQueryMethod {
 
-    @Language(
-            value = "JAVA",
-            prefix = "class BatchQueryMethods { java.util.Map ",
-            suffix = "(java.util.Collection keys) { return null; } }"
-    )
     String batchMethod();
 }
